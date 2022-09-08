4f6370cba0ed0c7de1daab301574a415203a26c4
anasidorovich <anasidorovich@gmail.com>
2022-09-08 16:41:51 +0300

    feat: add newsletters

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,22 @@
-import Swiper from 'swiper';
-import 'swiper';
-const swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-});
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var items = document.getElementsByClassName("carousel__item");
+    var dots = document.querySelectorAll(".carousel__indicators button");
+    if (n > items.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = items.length}
+    for (i = 0; i < items.length; i++) {
+        items[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    items[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " active";
+}
