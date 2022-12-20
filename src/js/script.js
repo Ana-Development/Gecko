6243f39e54d588ec9526996690ef11f565816f1b
Nastya Sidorovich <e9003447@ltfinc.net>
2022-12-21 02:31:25 +0300

    feat: update slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 1;
+//showDivs(slideIndex);
 
 function currentDiv(n) {
     showDivs(slideIndex = n);
@@ -9,14 +9,32 @@ function showDivs(n) {
     var i;
     var items = document.getElementsByClassName("carousel__item");
     var dots = document.querySelectorAll(".carousel__indicators button");
-    if (n > items.length) {slideIndex = 1}
+    if (n > items.length) {slideIndex = 1};
+    console.log(slideIndex)
     if (n < 1) {slideIndex = items.length}
     for (i = 0; i < items.length; i++) {
-        items[i].style.display = "none";
+        //items[i].style.display = "none";
+        items[i].classList.remove("carousel__item_active");
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        //dots[i].className = dots[i].className.replace(" active", "");
     }
-    items[slideIndex-1].style.display = "flex";
+    //items[slideIndex-1].style.display = "flex";
+    items[slideIndex-1].classList.add("carousel__item_active");
     dots[slideIndex-1].className += " active";
+}
+
+let slideShowIndex = 0;
+showSlides();
+
+function showSlides() {
+    if (slideShowIndex > 4) {
+        slideShowIndex = 0;
+    }
+    slideShowIndex++;
+    if (slideShowIndex >= 0) {
+        currentDiv(slideShowIndex);
+    }
+
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
