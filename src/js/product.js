322f35a70ce62afcb98e534ac39efeccea4ff286
Nastya Sidorovich <e9003447@ltfinc.net>
2022-12-28 23:34:20 +0300

    feat: add product show more popup

---
+++ src/js/product.js
@@ -0,0 +1,11 @@
+const showMorePopup = document.querySelector(".product__show-more");
+const showMoreBtn = document.querySelector(".product__more-img");
+const closeMoreBtn = document.querySelector(".product__show-more-img");
+
+showMoreBtn.addEventListener("click", function () {
+    showMorePopup.style.display = "block";
+});
+closeMoreBtn.addEventListener("click", function () {
+    showMorePopup.style.display = "none";
+    showMorePopup.parentElement.scrollIntoView();
+});
